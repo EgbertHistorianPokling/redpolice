d0340a27484925823ef760d814d7816101342a4a
ye <549127129@qq.com>
2018-06-18 18:19:48 +0800

    add sprite move and collision detection

--- Classes/GamePlayScene.cpp
+++ Classes/GamePlayScene.cpp
@@ -3,11 +3,15 @@
 
 USING_NS_CC;
 
+
 Scene* GamePlayScene::createScene()
 {
 	// 'scene' is an autorelease object
 
-	auto scene = Scene::create();
+	auto scene = Scene::createWithPhysics();
+
+	scene->getPhysicsWorld()->setGravity(Vec2(0,0));        //set Gravity
+	//scene->getPhysicsWorld()->setAutoStep(false);
 
 	// 'layer' is an autorelease object
 
@@ -30,29 +34,73 @@ bool GamePlayScene::init()
 		return false;
 	}
 
-
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	Size winSize= Director::getInstance()->getWinSize();
 
 	map = TMXTiledMap::create("map.tmx");                     // map
 	addChild(map,0,100);
 
+	Size size0 = map->getContentSize();
+
 	TMXObjectGroup *group = map->getObjectGroup("objects");
-	ValueMap spawnPoint = group->getObject("InitialCar");
+	auto spawnPoint = group->getObjects();
+	
+	for (auto it = spawnPoint.begin(); it != spawnPoint.end(); it++)
+	{
+		auto vm = it->asValueMap();
+		float x = vm["x"].asFloat();
+		float y = vm["y"].asFloat();
 
-	float x = spawnPoint["x"].asFloat();
-	float y = spawnPoint["y"].asFloat();
-
-	InitialCar = Sprite::create("InitialCar.png");           //initial sprite
-	InitialCar->setPosition(Vec2(x, y));
-	addChild(InitialCar, 2, 200);
-
+		auto sprite = Sprite::create("barrier.png");
+		sprite->setTag(1);
+		auto bd = PhysicsBody::createBox(sprite->getContentSize());
+		bd->setContactTestBitmask(0xFFFFFFFF);
+		sprite->setPhysicsBody(bd);
+		PhysicsMaterial(0, 0, 0);
+		bd->setDynamic(false);
+		sprite->setVisible(false);
+		sprite->setPosition(Vec2(x, y));
+		addChild(sprite, 2);
+	}
 	collidable = map->getLayer("collidable");
-	collidable->setVisible(false);                        //hide collidable
+	//collidable->setVisible(false);                        //hide collidable
 
 	setTouchEnabled(true);                    
 	setTouchMode(Touch::DispatchMode::ONE_BY_ONE);               //set single point touch
 	
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->setSwallowTouches(true);
+	listener->onTouchBegan = CC_CALLBACK_2(GamePlayScene::onTouchBegan, this);
+	listener->onTouchMoved = CC_CALLBACK_2(GamePlayScene::onTouchMoved, this);
+	listener->onTouchEnded = CC_CALLBACK_2(GamePlayScene::onTouchEnded, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+
+
+	std::string images[] = { "InitialCar.png" };         //sprite array
+	int images_length = 1;
+	for (int i = 0; i < images_length; ++i)
+	{
+		std::string image = images[i];
+		Sprite *sprite = Sprite::create(image);
+
+		sprite->setTag(1);
+		auto body = PhysicsBody::createBox(sprite->getContentSize());
+		body->setContactTestBitmask(0xFFFFFFFF);
+		sprite->setPhysicsBody(body);
+
+		//float offsetFraction = ((float)(i + 1)) / (images_length + 1);
+		//sprite->setPosition(visibleSize.width*offsetFraction, visibleSize.height / 2);
+		this->addChild(sprite,2);
+
+		movableSprites.pushBack(sprite);
+	}
+
+	movableSprites.front()->setPosition(Vec2(300, 200));
+
+
+	scheduleUpdate();               //Open the default timer. The refresh interval is one frame.
+
 	return true;
 };
 
@@ -60,60 +108,56 @@ bool GamePlayScene::init()
 
 bool GamePlayScene::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event)
 {
-	log("onTouchBegan");
+	touchLocation = this->convertTouchToNodeSpace(touch);
+	this->selectSpriteForTouch(touchLocation);
+
+	/*if (selSprite != nullptr)
+	{
+		Vec2 spritePos = selSprite->getPosition();
+		Vec2 tileCoord = this->tileCoordFromPosition(touchLocation);
+		int tileGid = collidable->getTileGIDAt(tileCoord);
+		if (tileGid == 0)
+			ActionInterval* actionTo = MoveTo::create(diff/30, touchLocation);
+			selSprite->runAction(actionTo);
+	}*/
 	return true;
 };
 
 
 void GamePlayScene::onTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event)
 {
-	log("onTouchMoved");
+
 };
 
 
 void GamePlayScene::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event)
 {
-	log("onTouchEnded");
+	
+};
 
-	Vec2 touchLocation = touch->getLocation();
-
-	Vec2 spritePos = InitialCar->getPosition();
-	Vec2 diff = touchLocation - spritePos;
-
-	if (abs(diff.x) > abs(diff.y))
+void GamePlayScene::update(float dt)
+{
+	if (selSprite != nullptr)
 	{
-		if (diff.x > 0)
+		Vec2 spritePos = selSprite->getPosition();
+		//dis = spritePos.distance(touchLocation);
+		Vec2 sub = touchLocation - spritePos;
+		float dis = sub.length();
+		sub.normalize();
+		if (dis>1)
 		{
-			spritePos.x += map->getTileSize().width;
-			InitialCar->runAction(FlipX::create(false));
+			selSprite->setPosition(Vec2(spritePos + sub));
 		}
-		else
-		{
-			spritePos.x -= map->getTileSize().width;
-			InitialCar->runAction(FlipX::create(true));
-		}
+		this->setViewpointCenter(spritePos);
 	}
-	else
-	{
-		if (diff.y > 0)
-		{
-			spritePos.y += map->getTileSize().height;
-		}
-		else
-		{
-			spritePos.y -= map->getTileSize().height;
-		}
-	}
-	InitialCar->setPosition(spritePos);
-	this->setViewpointCenter(spritePos);
-	//this->setSpritePosition(spritePos);
-}
+};
 
-/*void GamePlayScene::setSpritePosition(cocos2d::Vec2 position)           //move sprite and detect collision 
+
+void GamePlayScene::collisionWithMap(cocos2d::Vec2 position)           //detect collision in map 
 {
 	Vec2 tileCoord = this->tileCoordFromPosition(position);
 	int tileGid = collidable->getTileGIDAt(tileCoord);
-	if (tileGid == 0)
+	if (tileGid =! 0)
 	{
 		Value prop = map->getPropertiesForGID(tileGid);
 		ValueMap propValueMap = prop.asValueMap();
@@ -126,15 +170,18 @@ void GamePlayScene::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event)
 			return;
 		}
 	}
-		InitialCar->setPosition(position);
-}*/
+};
+
 
 Vec2 GamePlayScene::tileCoordFromPosition(Vec2 pos)         //from pixel position to tile position
 {
+
 	int x = pos.x / map->getTileSize().width;
-	int y = ((map->getMapSize().height*map->getTileSize().height) - pos.y) / map->getTileSize().height;
+	int y = (map->getMapSize().height*map->getTileSize().height - pos.y ) / map->getTileSize().height;
+
 	return(Vec2(x, y));
-}
+};
+
 
 void GamePlayScene::setViewpointCenter(Vec2 position)       //make sprite in the map center
 {
@@ -153,3 +200,85 @@ void GamePlayScene::setViewpointCenter(Vec2 position)       //make sprite in the
 
 	this -> setPosition(offset);
 };
+
+void GamePlayScene::selectSpriteForTouch(Point touchLocation)          // some actions about selected sprites 
+{
+	Sprite * newSprite = nullptr;
+	for (Sprite *sprite : movableSprites)
+	{
+		if (sprite->getBoundingBox().containsPoint(touchLocation))
+		{
+			newSprite = sprite;
+			break;
+		}
+	}
+
+	if ((newSprite!=selSprite) &&(newSprite!=nullptr))
+	{
+		if (selSprite!=nullptr)
+		{
+			selSprite->stopAllActions();
+			selSprite->runAction(RotateTo::create(0.1, 0));
+		}
+
+		RotateBy * rotLeft = RotateBy::create(0.1, -14.0);
+		RotateBy * rotCenter = RotateBy::create(0.1, 0.0);
+		RotateBy * rotRight = RotateBy::create(0.1, 14.0);
+		Sequence * rotSeq = Sequence::create(rotLeft, rotCenter, rotRight, rotCenter, NULL);
+		newSprite->runAction(RepeatForever::create(rotSeq));
+		selSprite = newSprite;
+	}
+};
+
+
+void GamePlayScene::onEnter()
+{
+	Layer::onEnter();
+	auto listener = EventListenerPhysicsContact::create();
+	listener->onContactBegin = [](PhysicsContact& contact)                           
+	{
+		auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();                
+		auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();                
+
+		return true;
+	};
+
+
+	listener->onContactPreSolve = [](PhysicsContact& contact,
+		PhysicsContactPreSolve& solve) 
+	{                
+		return true;
+	};
+
+
+	listener->onContactPostSolve = [](PhysicsContact& contact,
+		const PhysicsContactPostSolve& solve) 
+	{              
+		return true;
+	};
+
+
+	listener->onContactSeparate = [](PhysicsContact& contact)
+	{
+		auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
+		auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
+
+		if (spriteA && spriteA->getTag() == 1
+			&& spriteB && spriteB->getTag() == 1)
+		{
+			spriteA->setColor(Color3B::WHITE);
+			spriteB->setColor(Color3B::WHITE);
+		}
+	};
+
+
+Director::getInstance()->getEventDispatcher()->addEventListenerWithFixedPriority(listener, 1);                  
+
+}
+
+
+void GamePlayScene::onExit()
+{
+	Layer::onExit();
+	Director::getInstance()->getEventDispatcher()->removeAllEventListeners();             
+}
--- Classes/GamePlayScene.h
+++ Classes/GamePlayScene.h
@@ -10,24 +10,38 @@ class GamePlayScene :public cocos2d::Layer
 	cocos2d::TMXTiledMap *map;
 	cocos2d::TMXLayer *collidable;
 	cocos2d::Sprite* InitialCar;
-	
+	cocos2d::Vec2 touchLocation;
+
 public:
+
+	cocos2d::Sprite * selSprite;
+	cocos2d::Sprite * sprite0;
+	cocos2d::Vector<cocos2d::Sprite *> movableSprites;
+	
 	static cocos2d::Scene *createScene();
+
 	virtual bool init();
+	virtual void onEnter();
+	virtual void onExit();
 
 	virtual bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event);
 	virtual void onTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event);
 	virtual void onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event);
 
-	//void setSpritePosition(cocos2d::Vec2 position);
+	virtual void update(float dt);
+
+	void collisionWithMap(cocos2d::Vec2 position);
 
 	void setViewpointCenter(cocos2d::Vec2 position);
 
 	cocos2d::Vec2 tileCoordFromPosition(cocos2d::Vec2 position);
 
+	void selectSpriteForTouch(cocos2d::Point touchLocation);
+
+	cocos2d::Vec2 tilePosFromLocation(cocos2d::CCPoint location, cocos2d::CCTMXTiledMap *tilemap);
+
 	CREATE_FUNC(GamePlayScene);
 };
-
 
 
 #endif // _GAMEPLAYSCENE_H_
